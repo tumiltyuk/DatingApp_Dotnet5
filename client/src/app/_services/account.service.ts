ddbc20efddcd99f32548e7d9a8fc3cb5bd072e74
Robert Tumilty <rob.Tumilty@gmail.com>
2020-10-06 11:58:29 +0100

    End of Section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  // 'ReplaySubject' is like a buffer object - store values 
+  // any time someone subscribes to this object the ReplaySubject will omit the last values in the buffer
+  // '(1)' is recording how many items in the buffer
+  private currentUserSource = new ReplaySubject<User>(1);
+  // '$' is used by convention to say that the variable is an observable
+  currentUser$ = this.currentUserSource.asObservable();
+
+constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+
+}
